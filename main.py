0e81605a1f95d0e66414c362cd8271ac70328812
Daniil Boiko <daniel.boyko.al@gmail.com>
2018-06-14 15:57:42 +0300

    Initial file format change
    Now it can work with compass files, without excel step

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 inputfile = str(input('Имя исходного файла: '))
 outputfile = str(input('Имя конеченого файла: '))
 our_file = open(inputfile)
-results = open(outputfile,'w')
+results = open(outputfile, 'w')
 lowest = float(input('Начальная масса: '))
 highest = float(input('Конечная масса: '))
 for line in our_file.readlines():
-    strline = str(line).split(';')
+    strline = str(line).split(',')
     time = strline[0]
     print(time)
     mass = 0
     sum_int = 0
-    for i in range(1, int((len(strline) - 1) / 2) + 1):
-        if strline[2*i-1] != '' and strline[2*i] != '':
-            mass = float(strline[2*i-1])
-            intens = float(strline[2*i])
-            if (mass <= highest) and (mass >= lowest):
-                sum_int += intens
-            try:
-                time = float(time)
-            except:
-                print('error')
+    for i in range(8, len(strline)):
+        data = strline[i].split(' ')
+        mass = float(data[0])
+        intens = float(data[1])
+        if (mass <= highest) and (mass >= lowest):
+            sum_int += intens
+        try:
+            time = float(time)
+        except:
+            print('error')
 
-    results.write(str(time)+';'+str(sum_int)+'\n')
+    results.write(str(time) + ';' + str(sum_int) + '\n')
 
 our_file.close()
 results.close()
